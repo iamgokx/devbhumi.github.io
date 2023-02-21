e40777b09431a7cf51a7b463f5103576879f81ce
Gokul Lekhwar <lekhwargokul84@gmail.com>
2023-02-21 20:43:05 +0530

    updated 2

--- main.js
+++ main.js
@@ -4,12 +4,53 @@ let navbtn = document.getElementById('navbtn')
 flag = 0
 
 function togglenav() {
-    if( flag == 0){
+    if (flag == 0) {
         navbar.style.height = "15rem"
         flag = 1
-    }else if(flag == 1){
+    } else if (flag == 1) {
         navbar.style.height = "4rem"
         flag = 0
     }
 }
+
+
+
+let box1 = document.getElementById('box1')
+let box2 = document.getElementById('box2')
+let box3 = document.getElementById('box3')
+let box4 = document.getElementById('box4')
+
+box1.addEventListener('mouseenter', () => {
+    box1.style.scale = "1.1"
+})
+
+box1.addEventListener('mouseleave', () => {
+    box1.style.scale = "1"
+})
+
+
+box2.addEventListener('mouseenter', () => {
+    box2.style.scale = "1.1"
+})
+
+box2.addEventListener('mouseleave', () => {
+    box2.style.scale = "1"
+})
+
+box3.addEventListener('mouseenter', () => {
+    box3.style.scale = "1.1"
+})
+
+box3.addEventListener('mouseleave', () => {
+    box3.style.scale = "1"
+})
+
+box4.addEventListener('mouseenter', () => {
+    box4.style.scale = "1.1"
+})
+
+box4.addEventListener('mouseleave', () => {
+    box4.style.scale = "1"
+})
+
 
